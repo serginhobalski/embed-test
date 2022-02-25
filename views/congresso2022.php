7d2b607bc618bc18d8303aa2ca684df00b3c3f96
serginhobalski <serginhobalski@gmail.com>
2022-02-25 16:59:32 -0300

    53 files added

---
+++ views/congresso2022.php
@@ -0,0 +1,147 @@
+  <!-- page title -->
+  <section class="page-title-section overlay" data-background="images/backgrounds/page-title.jpg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <ul class="list-inline custom-breadcrumb">
+            <li class="list-inline-item"><a class="h2 text-primary font-secondary" href="events.html">Próximos Eventos</a></li>
+            <li class="list-inline-item text-white h3 font-secondary nasted">Congresso Estadual de Educação</li>
+          </ul>
+          <p class="text-lighten">O congresso de todos os educadores quadrangulares do Pará</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /page title -->
+
+  <!-- event single -->
+  <section class="section-sm">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="section-title">Força e Coragem para Crescer e Multiplicar</h2>
+        </div>
+        <!-- event image -->
+        <div class="col-12 mb-4">
+          <img src="images/events/event-single.jpg" alt="event thumb" class="img-fluid w-100">
+        </div>
+      </div>
+      <!-- event info -->
+      <div class="row align-items-center mb-5">
+        <div class="col-lg-9">
+          <ul class="list-inline">
+            <li class="list-inline-item mr-xl-5 mr-4 mb-3 mb-lg-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-location-pin text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">LOCAL</h6>
+                  <p class="mb-0">Belém, PA</p>
+                </div>
+              </div>
+            </li>
+            <li class="list-inline-item mr-xl-5 mr-4 mb-3 mb-lg-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-calendar text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">DATA</h6>
+                  <p class="mb-0">08 de Agosto (sábado)</p>
+                </div>
+              </div>
+            </li>
+            <li class="list-inline-item mr-xl-5 mr-4 mb-3 mb-lg-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-time text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">HORA</h6>
+                  <p class="mb-0">08:00 - 18:00</p>
+                </div>
+              </div>
+            </li>
+            <li class="list-inline-item mr-xl-5 mr-4 mb-3 mb-lg-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-wallet text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">INSCRIÇÃO</h6>
+                  <p class="mb-0">Em breve...</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-3 text-lg-right text-left">
+          <a href="#" class="btn btn-primary">Aguarde...</a>
+        </div>
+        <!-- border -->
+        <div class="col-12 mt-4 order-4">
+          <div class="border-bottom border-primary"></div>
+        </div>
+      </div>
+      <!-- event details --
+    <div class="row">
+      <div class="col-12 mb-50">
+        <h3>About Event</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+          anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+          consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+          ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+          labore et dolore magnam aliquam quaerat voluptatem.</p>
+      </div>
+    </div>
+    <!-- event speakers -->
+      <div class="row">
+        <div class="col-12">
+          <h3 class="mb-4">Palestrantes</h3>
+        </div>
+        <!-- speakers -->
+        <div class="col-lg-3 col-sm-6 mb-4 mb-lg-0">
+          <div class="media">
+            <img class="mr-3 img-fluid" src="images/event-speakers/eurides.jpg" alt="speaker">
+            <div class="media-body">
+              <h4 class="mt-0">Eurides Obalski</h4>
+              Diretora SEEC-PA
+            </div>
+          </div>
+        </div>
+        <!-- speakers -->
+        <div class="col-lg-3 col-sm-6 mb-4 mb-lg-0">
+          <div class="media">
+            <img class="mr-3 img-fluid" src="images/event-speakers/speaker-2.jpg" alt="speaker">
+            <div class="media-body">
+              <h4 class="mt-0">Palestrante</h4>
+              A ser confirmado(a)
+            </div>
+          </div>
+        </div>
+        <!-- speakers -->
+        <div class="col-lg-3 col-sm-6 mb-4 mb-lg-0">
+          <div class="media">
+            <img class="mr-3 img-fluid" src="images/event-speakers/speaker-3.jpg" alt="speaker">
+            <div class="media-body">
+              <h4 class="mt-0">Palestrante</h4>
+              A ser confirmado(a)
+            </div>
+          </div>
+        </div>
+        <!-- speakers -->
+        <div class="col-lg-3 col-sm-6 mb-4 mb-lg-0">
+          <div class="media">
+            <img class="mr-3 img-fluid" src="images/event-speakers/speaker-4.jpg" alt="speaker">
+            <div class="media-body">
+              <h4 class="mt-0">Palestrante</h4>
+              A ser confirmado(a)
+            </div>
+          </div>
+        </div>
+        <!-- border -->
+        <div class="col-12 mt-4 order-4">
+          <div class="border-bottom border-primary"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /event single -->
