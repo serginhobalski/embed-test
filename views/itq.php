9a9c22bf3c663d520e661b529c0ba79f9bf34d4b
serginhobalski <serginhobalski@gmail.com>
2022-02-25 17:14:33 -0300

    57 files added

---
+++ views/itq.php
@@ -0,0 +1,282 @@
+  <!-- page title -->
+  <section class="page-title-section overlay" data-background="images/backgrounds/page-title.jpg">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <ul class="list-inline custom-breadcrumb">
+            <li class="list-inline-item"><a class="h2 text-primary font-secondary" href="courses.html">Cursos</a></li>
+            <li class="list-inline-item text-white h3 font-secondary nasted">ITQ EAD</li>
+          </ul>
+          <p class="text-lighten">Ensino teológico de qualidade ao seu alcance em qualquer lugar.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /page title -->
+
+  <!-- section -->
+  <section class="section-sm">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mb-4">
+          <!-- course thumb -->
+          <img src="images/courses/itq-poster(1165x583).jpg" class="img-fluid w-100">
+        </div>
+      </div>
+      <!-- course info -->
+      <div class="row align-items-center mb-5">
+        <div class="col-xl-3 order-1 col-sm-6 mb-4 mb-xl-0">
+          <h2>ITQ EAD</h2>
+        </div>
+        <div class="col-xl-6 order-sm-3 order-xl-2 col-12 order-2">
+          <ul class="list-inline text-xl-center">
+            <li class="list-inline-item mr-4 mb-3 mb-sm-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-book text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">1º ao 3º ANO</h6>
+                  <p class="mb-0">06 Módulo /ano</p>
+                </div>
+              </div>
+            </li>
+            <li class="list-inline-item mr-4 mb-3 mb-sm-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-alarm-clock text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">DURATION</h6>
+                  <p class="mb-0">03 Hours</p>
+                </div>
+              </div>
+            </li>
+            <li class="list-inline-item mr-4 mb-3 mb-sm-0">
+              <div class="d-flex align-items-center">
+                <i class="ti-wallet text-primary icon-md mr-2"></i>
+                <div class="text-left">
+                  <h6 class="mb-0">FEE</h6>
+                  <p class="mb-0">From: $699</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="col-xl-3 text-sm-right text-left order-sm-2 order-3 order-xl-3 col-sm-6 mb-4 mb-xl-0">
+          <a href="#" class="btn btn-primary">Matricule-se já</a>
+        </div>
+        <!-- border -->
+        <div class="col-12 mt-4 order-4">
+          <div class="border-bottom border-primary"></div>
+        </div>
+      </div>
+      <!-- course details -->
+      <div class="row">
+        <div class="col-12 mb-4">
+          <h3>Instituto Teológico Quadrangular EAD</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+            consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+            labore et dolore magnam aliquam quaerat voluptatem.</p>
+        </div>
+        <div class="col-12 mb-4">
+          <h3 class="mb-3">Requisitos</h3>
+          <div class="col-12 px-0">
+            <div class="row">
+              <div class="col-md-6">
+                <ul class="list-styled">
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+                <ul class="list-styled">
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 mb-4">
+          <h3 class="mb-3">Como se matricular</h3>
+          <ul class="list-styled">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
+              dolor. Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
+              dolor. Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
+              dolor. Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
+              dolor. Lorem, ipsum.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae obcaecati unde nulla? Lorem, ipsum
+              dolor. Lorem, ipsum.</li>
+          </ul>
+        </div>
+        <div class="col-12 mb-5">
+          <h3>Investimento</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+            consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+            labore et dolore magnam aliquam quaerat voluptatem.</p>
+        </div>
+        <!-- teacher -->
+        <div class="col-12">
+          <h5 class="mb-3">Teacher</h5>
+          <div class="d-flex justify-content-between align-items-center flex-wrap">
+            <div class="media mb-2 mb-sm-0">
+              <img class="mr-4 img-fluid" src="images/teacher.jpg" alt="Teacher">
+              <div class="media-body">
+                <h4 class="mt-0">Sam Somrat</h4>
+                Photographer
+              </div>
+            </div>
+            <div class="social-link">
+              <h6 class="d-none d-sm-block">Social Link</h6>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a class="d-inline-block text-light p-1" href="#"><i class="ti-facebook"></i></a></li>
+                <li class="list-inline-item"><a class="d-inline-block text-light p-1" href="#"><i class="ti-twitter-alt"></i></a></li>
+                <li class="list-inline-item"><a class="d-inline-block text-light p-1" href="#"><i class="ti-linkedin"></i></a></li>
+                <li class="list-inline-item"><a class="d-inline-block text-light p-1" href="#"><i class="ti-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="border-bottom border-primary mt-4"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /section -->
+
+  <!-- related course -->
+  <section class="section pt-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="section-title">Grades de disciplinas</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <!-- course item -->
+        <div class="col-lg-4 col-sm-6 mb-5">
+          <div class="card p-0 border-primary rounded-0 hover-shadow">
+            <img class="card-img-top rounded-0" src="images/courses/course-1.jpg" alt="course thumb">
+            <div class="card-body">
+              <ul class="list-inline mb-2">
+                <li class="list-inline-item"><i class="ti-calendar mr-1 text-color"></i>06 Módulos</li>
+                <li class="list-inline-item"><a class="text-color" href="#">ITQ EAD</a></li>
+              </ul>
+              <a href="course-single.html">
+                <h4 class="card-title">1º ANO</h4>
+              </a>
+              <p class="card-text mb-4">
+              <ul>
+                <li>Metodologia do Trabalho Acadêmico</li>
+                <li>Vivência Cristã</li>
+                <li>Bibliologia</li>
+                <li>Introdução à Teologia</li>
+                <li>Dons e Ministérios</li>
+                <li>Pastoral Urbana</li>
+                <li>Vida de Cristo e Espiritualidade</li>
+                <li>Discipulado</li>
+                <li>Evangelho Quadrangular</li>
+                <li>Evangelismo</li>
+                <li>Administração Eclesiática</li>
+                <li>Hermenêutica</li>
+                <li>Introdução à Educação Cristã</li>
+                <li>Aconselham. e Orientação Familiar</li>
+                <li>Homilética</li>
+                <li>Métodos de Estudo Bíblico</li>
+                <li>Liderança 1</li>
+                <li>Teologia do Culto</li>
+                <li>Teologia Pastoral</li>
+              </ul>
+              </p>
+              <a href="course-single.html" class="btn btn-primary btn-sm">Matricule-se</a>
+            </div>
+          </div>
+        </div>
+        <!-- course item -->
+        <div class="col-lg-4 col-sm-6 mb-5">
+          <div class="card p-0 border-primary rounded-0 hover-shadow">
+            <img class="card-img-top rounded-0" src="images/courses/course-2.jpg" alt="course thumb">
+            <div class="card-body">
+              <ul class="list-inline mb-2">
+                <li class="list-inline-item"><i class="ti-calendar mr-1 text-color"></i>06 Módulos</li>
+                <li class="list-inline-item"><a class="text-color" href="#">ITQ EAD</a></li>
+              </ul>
+              <a href="course-single.html">
+                <h4 class="card-title">2º ANO</h4>
+              </a>
+              <p class="card-text mb-4">
+              <ul>
+                <li>Cultura Bíblica</li>
+                <li>Teologia do Antigo Testamento</li>
+                <li>Pentateuco (I e II)</li>
+                <li>Teologia do Novo Testamento</li>
+                <li>Hebreus</li>
+                <li>Evangelhos (I e II)</li>
+                <li>Atos dos Apóstolos</li>
+                <li>História de Israel (I, II e III)</li>
+                <li>Epístolas Paulinas (I e II)</li>
+                <li>Epístolas Gerais</li>
+                <li>Livros Poéticos</li>
+                <li>Missão Integral da Igreja</li>
+                <li>Teologia Sistemática 1</li>
+              </ul>
+              </p>
+              <a href="course-single.html" class="btn btn-primary btn-sm">Matricule-se</a>
+            </div>
+          </div>
+        </div>
+        <!-- course item -->
+        <div class="col-lg-4 col-sm-6 mb-5">
+          <div class="card p-0 border-primary rounded-0 hover-shadow">
+            <img class="card-img-top rounded-0" src="images/courses/course-3.jpg" alt="course thumb">
+            <div class="card-body">
+              <ul class="list-inline mb-2">
+                <li class="list-inline-item"><i class="ti-calendar mr-1 text-color"></i>06 Módulos</li>
+                <li class="list-inline-item"><a class="text-color" href="#">ITQ EAD</a></li>
+              </ul>
+              <a href="course-single.html">
+                <h4 class="card-title">3º ANO</h4>
+              </a>
+              <p class="card-text mb-4">
+              <ul>
+                <li>TCC</li>
+                <li>Teologia Sistemática 2 (I e II)</li>
+                <li>Estágio Supervisionado</li>
+                <li>História da Igreja (I e II)</li>
+                <li>Aconselhamento Pastoral</li>
+                <li>Teologia Sistemática 3 (I e II)</li>
+                <li>Escatologia Bíblica (I e II)</li>
+                <li>Liderança 2</li>
+                <li>Introdução à Missiologia</li>
+                <li>Mov. Religiosos Contemporâneos</li>
+                <li>Liderança 3</li>
+                <li>Comunicação Social</li>
+                <li>Teologia Contemporânea</li>
+                <li>Ética</li>
+              </ul>
+              </p>
+              <a href="course-single.html" class="btn btn-primary btn-sm">Matricule-se</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /related course -->
